fc5651506648469fbfbdfa0902e681325a0f7a1e
Mudz-Dev <grayfoxnz@outlook.co.nz>
2021-11-29 20:50:59 +1300

    add hotkey functionality

--- AudioDeviceChanger/MainWindow.xaml.cs
+++ AudioDeviceChanger/MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
 
 namespace AudioDeviceChanger
 {
@@ -22,6 +24,28 @@ namespace AudioDeviceChanger
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private const int ADDHOTKEY_ID = 9000;
+        private const int SUBHOTKEY_ID = 9001;
+
+        //Modifiers:
+        private const uint MOD_NONE = 0x0000; //(none)
+        private const uint MOD_ALT = 0x0001; //ALT
+        private const uint MOD_CONTROL = 0x0002; //CTRL
+        private const uint MOD_SHIFT = 0x0004; //SHIFT
+        private const uint MOD_WIN = 0x0008; //WINDOWS
+        
+        //Add Key:
+        private const uint VK_ADD = 0x6B;
+
+        //Subtract Key:
+        private const uint VK_SUB = 0x6D;
 
         AudioDevices Audio { get; set; }
         public MainWindow()
@@ -67,6 +91,62 @@ namespace AudioDeviceChanger
         {
             Audio.DecrementPlaybackDevice();
             RefreshDevices();
+        }
+
+        private IntPtr _windowHandle;
+        private HwndSource _source;
+        private void Window_SourceInitialized(object sender, EventArgs e)
+        {
+
+            _windowHandle = new WindowInteropHelper(this).Handle;
+            _source = HwndSource.FromHwnd(_windowHandle);
+            _source.AddHook(HwndHook);
+
+            RegisterHotKey(_windowHandle, ADDHOTKEY_ID, MOD_CONTROL, VK_ADD); //CTRL + ADD
+            RegisterHotKey(_windowHandle, SUBHOTKEY_ID, MOD_CONTROL, VK_SUB); //CTRL + SUB
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            _source.RemoveHook(HwndHook);
+            UnregisterHotKey(_windowHandle, ADDHOTKEY_ID);
+            UnregisterHotKey(_windowHandle, SUBHOTKEY_ID);
+            base.OnClosed(e);
+        }
+
+        private IntPtr HwndHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            int vkey;
+            const int WM_HOTKEY = 0x0312;
+            switch (msg)
+            {
+                case WM_HOTKEY:
+                    switch (wParam.ToInt32())
+                    {
+                        case ADDHOTKEY_ID:
+                            vkey = (((int)lParam >> 16) & 0xFFFF);
+                            if (vkey == VK_ADD)
+                            {
+                                Console.WriteLine("Add Key Combo Pressed");
+                                Audio.IncrementPlaybackDevice();
+                                RefreshDevices();
+                            }
+                            handled = true;
+                            break;
+                        case SUBHOTKEY_ID:
+                            vkey = (((int)lParam >> 16) & 0xFFFF);
+                            if (vkey == VK_SUB)
+                            {
+                                Console.WriteLine("Subtract Key Combo Pressed");
+                                Audio.DecrementPlaybackDevice();
+                                RefreshDevices();
+                            }
+                            handled = true;
+                            break;
+                    }
+                    break;
+            }
+            return IntPtr.Zero;
         }
     }
 }
